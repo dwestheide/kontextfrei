c7626935c443c931c37fe3aa8f798d8caa9d7571
Simon Scott <simonjscott@users.noreply.github.com>
2018-12-20 16:29:58 +0000

    fix call site reporting in Spark UI

--- core/src/main/scala/com/danielwestheide/kontextfrei/rdd/RDDBase.scala
+++ core/src/main/scala/com/danielwestheide/kontextfrei/rdd/RDDBase.scala
@@ -1,7 +1,25 @@
 package com.danielwestheide.kontextfrei.rdd
 
 import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
 
 private[kontextfrei] trait RDDBase {
   def sparkContext: SparkContext
+
+  def withSite[A, R](as: RDD[A])(body: RDD[A] => R): R = {
+
+    def isInKf(ste: StackTraceElement): Boolean = {
+      Option(ste.getClassName()).exists(_.startsWith("com.danielwestheide.kontextfrei"))
+    }
+
+    val (kf, user) = Thread.currentThread().getStackTrace()
+      .dropWhile(!isInKf(_))
+      .span(isInKf)
+
+    val method = kf.lastOption.map(_.getMethodName()).getOrElse("unknown")
+    val site = user.headOption.map(s => s"${s.getFileName()}:${s.getLineNumber()}").getOrElse("unknown")
+
+    as.sparkContext.setCallSite(s"$method at $site")
+    body(as)
+  }
 }
