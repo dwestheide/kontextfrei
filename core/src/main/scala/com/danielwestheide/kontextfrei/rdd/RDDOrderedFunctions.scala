75ade2b063d399dff468beb2079961f3d6caecf1
Simon Scott <simonjscott@users.noreply.github.com>
2019-01-15 09:27:49 +0000

    fix call site reporting in Spark UI for ordered functions

--- core/src/main/scala/com/danielwestheide/kontextfrei/rdd/RDDOrderedFunctions.scala
+++ core/src/main/scala/com/danielwestheide/kontextfrei/rdd/RDDOrderedFunctions.scala
@@ -5,18 +5,21 @@ import org.apache.spark.rdd.RDD
 import scala.reflect.ClassTag
 
 private[kontextfrei] trait RDDOrderedFunctions
-    extends DCollectionOrderedFunctions[RDD] {
+    extends DCollectionOrderedFunctions[RDD] { this: RDDBase =>
 
   override final def sortByKey[A: ClassTag: Ordering, B: ClassTag](
-      x: RDD[(A, B)])(ascending: Boolean): RDD[(A, B)] = x.sortByKey(ascending)
+      x: RDD[(A, B)])(ascending: Boolean): RDD[(A, B)] = withSite(x) {
+    _.sortByKey(ascending)
+  }
 
   override final def sortByKeyWithNumPartitions[A: ClassTag: Ordering,
                                                 B: ClassTag](
-      x: RDD[(A, B)])(ascending: Boolean, numPartitions: Int): RDD[(A, B)] =
-    x.sortByKey(ascending, numPartitions)
+      x: RDD[(A, B)])(ascending: Boolean, numPartitions: Int): RDD[(A, B)] = withSite(x) {
+    _.sortByKey(ascending, numPartitions)
+  }
 
   override final def filterByRange[A: ClassTag: Ordering, B: ClassTag](
-      x: RDD[(A, B)])(lower: A, upper: A): RDD[(A, B)] =
-    x.filterByRange(lower, upper)
-
+      x: RDD[(A, B)])(lower: A, upper: A): RDD[(A, B)] = withSite(x) {
+    _.filterByRange(lower, upper)
+  }
 }
